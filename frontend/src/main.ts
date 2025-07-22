15a42433e2865b69e8413ea8ebf139e9f16f439d
nevmstas <nevmyvakastas@gmail.com>
2025-07-22 15:22:24 +0600

    init shared lib

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,9 +1,12 @@
 import router from './router';
 import { createApp } from 'vue';
+import { VueQueryPlugin } from '@tanstack/vue-query'
 import App from './app/App.vue';
+
 
 import './index.css'
 
 const app = createApp(App);
+app.use(VueQueryPlugin)
 app.use(router);
 app.mount('#root');
---
+++ shared-types/src/lib/auth.schema.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const RegisterSchema = z.object({
+  email: z.email(),
+  username: z.string().min(2),
+  password: z.string().min(6),
+});
+
+export const LoginSchema = z.object({
+  email: z.email(),
+  password: z.string().min(6),
+});
+
+export type RegisterInput = z.infer<typeof RegisterSchema>;
+export type LoginInput = z.infer<typeof LoginSchema>;
